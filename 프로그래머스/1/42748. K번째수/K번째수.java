2139c5b4c5e20ad0293ef484f155ca4e71dffa5c
jihwanprogramer <130281249+jihwanprogramer@users.noreply.github.com>
2025-03-27 10:51:00 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,16 +1,18 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        for(int i=0;i<commands.length;i++){
+        for (int i = 0; i < commands.length; i++) {
             List<Integer> lists = new ArrayList<>();
-            for(int j=commands[i][0]-1;j<=commands[i][1]-1;j++){
+            // commands[i][1]까지 포함해야 하므로 '<='가 아니라 '<'로 수정
+            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
                 lists.add(array[j]);
             }
             Collections.sort(lists);
-            answer[i]=lists.get(commands[i][2]-1);
+            answer[i] = lists.get(commands[i][2] - 1);
         }
         return answer;
     }
