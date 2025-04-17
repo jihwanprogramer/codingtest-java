56f40a1d66bcbc348be3e0ed93de89f41f997bd4
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2025-04-17 12:52:10 +0900

    [level 1] Title: 소수 만들기, Time: 4.80 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,32 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] nums) {
-        List<Integer> lists = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                for(int l=j+1;l<nums.length;l++){
-                    int num = nums[i]+nums[j]+nums[l];
-                    lists.add(num);
+        int checknum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int l = j + 1; l < nums.length; l++) {
+                    int num = nums[i] + nums[j] + nums[l];
+                    boolean isNotPrime = false;
+                    for (int k = 2; k <= Math.sqrt(num); k++) {
+                        if (num % k == 0) {
+                            isNotPrime = true;
+                            break;
+                        }
+                    }
+                    if (!isNotPrime) {
+                        checknum++;
+                    }
                 }
             }
         }
-        
-        for(int i=lists.size()-1;i>=0;i--)
-        {
-             int count =0;
-            for(int j=1;j<=lists.get(i);j++){
-                if(lists.get(i)%j==0){
-                    count ++;
-                }
-         }
-            if(count!=2){
-                lists.remove(i);        
-            }
-            
-        }
-
-
-        return lists.size();
+        return checknum;
     }
 }
