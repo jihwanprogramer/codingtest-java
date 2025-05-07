e5fb7efb1733d6bbee2ea170e469bf74043a2eb0
KIM JI HWAN <130281249+jihwanprogramer@users.noreply.github.com>
2025-05-07 11:41:05 +0900

    [level 1] Title: 체육복, Time: 0.14 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,48 +2,51 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] clothes = new int[n];
-        Arrays.fill(clothes, 1);
-
-        // set 활용해 중복 제거
+        
+        int[] check = new int[n];
+        Arrays.fill(check,1);
+        
         Set<Integer> lostSet = new HashSet<>();
-        Set<Integer> reserveSet = new HashSet<>();
-
-        for (int l : lost) lostSet.add(l);
-        for (int r : reserve) {
-            if (lostSet.contains(r)) {
-                // 중복 제거: 도난 + 여벌 모두 있는 학생은 제외
-                lostSet.remove(r);
-            } else {
-                reserveSet.add(r);
+        Set<Integer> revSet = new HashSet<>();
+        
+        for(int i : lost){
+            lostSet.add(i);
+        }
+        
+        for(int s:reserve){
+            if(lostSet.contains(s)){
+                lostSet.remove(s);
+            }else{
+                revSet.add(s);
             }
         }
-
-        // 도난 반영
-        for (int l : lostSet) clothes[l - 1]--;
-
-        // 여벌 반영
-        for (int r : reserveSet) clothes[r - 1]++;
-
-        // 빌려주기 로직
-        for (int i = 0; i < n; i++) {
-            if (clothes[i] == 0) {
-                if (i > 0 && clothes[i - 1] == 2) {
-                    clothes[i]++;
-                    clothes[i - 1]--;
-                } else if (i < n - 1 && clothes[i + 1] == 2) {
-                    clothes[i]++;
-                    clothes[i + 1]--;
-                }
+        
+        for(int i: lostSet){
+            check[i-1] --;
+        }
+        
+        for(int s:revSet){
+            check[s-1] ++;
+        }
+        
+        for(int i=0; i< n ; i++){
+            if(check[i] == 0){
+               if(i>0 && check[i-1] == 2){
+                check[i-1]--;
+                check[i]++;
+              }else if (i<n-1 && check[i+1] == 2){
+                check[i+1]--;
+                check[i]++;
+               }
             }
         }
-
-        // 최종 수업 가능 인원 수
+        
         int answer = 0;
-        for (int c : clothes) {
+        for (int c : check) {
             if (c >= 1) answer++;
         }
-
+        
+        
         return answer;
-    }
+  }
 }
